18bd5c5e774d85bc6a4bd5605c070e116e99bb39
Todd DiFronzo <fonze@aol.com>
2020-08-09 18:09:12 -0700

    finished

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,28 @@
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
-    # Your code here
+    # base case
+       # we'll stop when we either find target,  
+       #  i.e, when left > right and we did not find the target
+    if start > end:
+        return -1
+    # how do we move closer to a base case?or we search the whole array
+    else:
+        mid = (start + end) // 2
+
+        if arr[mid] == target:
+            return mid
+
+        elif arr[mid] < target:
+            # rule out the left side
+            return binary_search(arr, target, mid+1, end)
+        else:
+            # rule out the right side
+            return binary_search(arr, target, start, mid-1) 
+
+arr = [3, 4, 6, 16, 26, 28, 52, 55]
+print(binary_search(arr, 52, 0, len(arr)-1))
+
+
 
 
 # STRETCH: implement an order-agnostic binary search
@@ -9,6 +31,47 @@ def binary_search(arr, target, start, end):
 # sorted in ascending order or in descending order
 # You can implement this function either recursively 
 # or iteratively
+
+# Descending binary search
+def descending_binary_search(arr, target, left, right):
+    if left > right:
+        return -1
+    else:
+        mid = (left + right) // 2
+
+        if arr[mid] == target:
+            return mid
+
+        elif arr[mid] < target:
+            # rule out the right side
+            return descending_binary_search(arr, target, left, mid-1)
+        else:
+            # rule out the left side
+            return descending_binary_search(arr, target, mid+1, right) 
+
+# Test it out
+
+# arr = [101, 99, 87, 76, 66, 65]
+# print('Desc: ', descending_binary_search(arr, 101, 0, len(arr)-1))
+
 def agnostic_binary_search(arr, target):
-    # Your code here
+    # figure out whether the input array is sorted in ascending or descending order
+    # keep a boolean to indicate the order of the array
+    # compare arr[0] and arr[1]
+    if arr[0] > arr[-1]:
+        is_ascending = False 
+    else:
+        is_ascending = True 
+
+    # if the input array is ascending, call our normal 'binary_search' with the array
+    #    and target
+    if is_ascending:
+        return binary_search(arr, target, 0, len(arr)-1)
+    # otherwise, call 'descending_binary_search' with the array and target
+    else:
+        return descending_binary_search(arr, target, 0, len(arr)-1)
+
+# Test it
+print("Agnostic Ascending: ",  agnostic_binary_search([1,3,5,6,8,21], 8))
+print("Agnostic Descending: ", agnostic_binary_search([101, 99, 87, 76, 66, 65], 66)) 
 
--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -1,19 +1,59 @@
 # TO-DO: complete the helper function below to merge 2 sorted arrays
 def merge(arrA, arrB):
-    elements = len(arrA) + len(arrB)
-    merged_arr = [0] * elements
+    num_elements = len(arrA) + len(arrB)
+    merged_arr = []
 
     # Your code here
+    a = 0  # pointer in A arry
+    b = 0  # pointer in B arry
 
+    while a < len(arrA) and b < len(arrB):
+        # check if arrA pointer is less than arrB pointer
+        if arrA[a] < arrB[b]:
+            merged_arr.append(arrA[a])
+            a += 1
+        else:
+            merged_arr.append(arrB[b])
+            b += 1
+    if a < len(arrA):
+        # arrA still has leftover elements
+        #   so push them all to the merged array
+        merged_arr.extend(arrA[a:])
+
+    if b < len(arrB):
+        merged_arr.extend(arrB[b:])
 
     return merged_arr
+
+# test it
+arrA = [3, 6, 8, 12, 15]
+arrB = [1, 4, 5, 9, 11]
+print("Merged array: ", merge(arrA, arrB))
+
+
+
+
 
 # TO-DO: implement the Merge Sort function below recursively
 def merge_sort(arr):
     # Your code here
+    # base case: stop spliting arrays in half when the arrays
+    #   reach length of 1 (single element arrays)
+    if len(arr) > 1: 
+    # otherwise, keep splitting them in half
+        left = merge_sort(arr[0 : len(arr) // 2])
+        right = merge_sort(arr[len(arr) // 2 : ])
+        return merge(left, right)
+
+    return arr 
+
+# test it
+arr = [45, 12, 89, 14, 67, 45, 23, 90, 11]
+print("Array sorted: ", merge_sort(arr))
 
 
-    return arr
+
+
 
 # STRETCH: implement the recursive logic for merge sort in a way that doesn't 
 # utilize any extra memory
@@ -21,8 +61,9 @@ def merge_sort(arr):
 # or data structures; it can only re-use the memory it was given as input
 def merge_in_place(arr, start, mid, end):
     # Your code here
-
+    pass 
+    
 
 def merge_sort_in_place(arr, l, r):
     # Your code here
-
+    pass 
